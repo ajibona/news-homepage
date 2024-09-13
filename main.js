e4800acb9acf7a5a0655f8643fa365e21ab6e4a7
ajibona <39883461+ajibona@users.noreply.github.com>
2024-09-13 07:50:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 
 const toggle = document.querySelector('.toggle')
+const toggleClose = document.querySelector('.toggle-close')
 const sidebar = document.querySelector('.sidebarid')
-const toggleclose = document.querySelector('.toggleclose');
 
+//menu icon open
 toggle.addEventListener('click', ()=>{
   toggle.classList.toggle('active')
+  sidebar.classList.toggle('activesidebar')
+})
+
+//menu icon close
+toggleClose.addEventListener('click', ()=>{
   sidebar.classList.toggle('activesidebar')
 })
